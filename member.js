787dd593dd4f5ccb7ae307bc20786e7d5776c30c
Enrico Danesi <95696843+henryd23@users.noreply.github.com>
2023-08-06 15:51:16 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    member.style.display = "block";
+    var skills = document.getElementById("skills");
+    skills.style.display = "none";
+    var project = document.getElementById("project");
+    project.style.display = "none";
+    var contact = document.getElementById("contact");
+    contact.style.display = "none";
+}
